a4d77bb13518a9fd7fe152938991299d4a34b4a1
tonymushah <tonyramanantsalama@gmail.com>
2025-03-07 00:04:14 +0300

    some lib updates

--- crates/sys/src/lib.rs
+++ crates/sys/src/lib.rs
@@ -1,13 +1,16 @@
-/// The equivalent of the [`window.__TAURI__.app`](https://v1.tauri.app/v1/api/js/app) module
+/// The equivalent of the [`window.__TAURI__.app`](https://v1.tauri.app/v1/api/js/app) module.
+///
 pub mod app;
-/// The equivalent of the [`window.__TAURI__.cli`](https://v1.tauri.app/v1/api/js/cli) module
+/// The equivalent of the [`window.__TAURI__.cli`](https://v1.tauri.app/v1/api/js/cli) module.
+///
 pub mod cli;
-/// The equivalent of the [`window.__TAURI__.clipboard`](https://v1.tauri.app/v1/api/js/clipboard) module
+/// The equivalent of the [`window.__TAURI__.clipboard`](https://v1.tauri.app/v1/api/js/clipboard) module.
+///
 pub mod clipboard;
 pub mod error;
-/// The equivalent of the [`window.__TAURI__.event`](https://v1.tauri.app/v1/api/js/event) module
+/// The equivalent of the [`window.__TAURI__.event`](https://v1.tauri.app/v1/api/js/event) module.
 pub mod event;
-/// The equivalent of the [`window.__TAURI__.tauri`](https://v1.tauri.app/v1/api/js/tauri) module
+/// The equivalent of the [`window.__TAURI__.tauri`](https://v1.tauri.app/v1/api/js/tauri) module.
 pub mod tauri;
 
 pub use error::Error;
