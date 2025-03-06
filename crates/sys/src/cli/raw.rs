2e80b0e48457aa623c795a9848ee95654bcc6a0e
tonymushah <tonyramanantsalama@gmail.com>
2025-03-06 12:06:28 +0300

    raw update

--- crates/sys/src/cli/raw.rs
+++ crates/sys/src/cli/raw.rs
@@ -3,6 +3,8 @@ use wasm_bindgen::prelude::*;
 #[wasm_bindgen(js_namespace = ["window","__TAURI__","cli"])]
 extern "C" {
     /// Parse the arguments provided to the current process and get the matches using the configuration defined [tauri.cli](https://tauri.app/v1/api/config/#tauriconfig.cli) in tauri.conf.json
+    ///
+    /// Since: 1.0.0
     #[wasm_bindgen(catch)]
     pub async fn getMatches() -> Result<JsValue, JsValue>;
 }
