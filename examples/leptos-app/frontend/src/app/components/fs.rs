1447fb90678be17312c624a5aef6e8a2155642a7
tonymushah <tonyramanantsalama@gmail.com>
2025-03-14 21:58:40 +0300

    added create dir test

--- examples/leptos-app/frontend/src/app/components/fs.rs
+++ examples/leptos-app/frontend/src/app/components/fs.rs
@@ -1,9 +1,11 @@
 pub mod base_directory;
 pub mod copy_file;
+pub mod create_dir;
 pub mod fs_dir_option;
 pub mod fs_option;
 
 use copy_file::CopyFile;
+use create_dir::CreateDir;
 use leptos::prelude::*;
 
 #[component]
@@ -13,6 +15,10 @@ pub fn Fs() -> impl IntoView {
             <section>
                 <h4>"Copy File"</h4>
                 <CopyFile />
+            </section>
+            <section>
+                <h4>"Create dir"</h4>
+                <CreateDir />
             </section>
         </div>
     }
---
+++ examples/leptos-app/frontend/src/app/components/fs/create_dir.rs
@@ -0,0 +1,72 @@
+use leptos::prelude::*;
+use tauri_global_sys::fs::{create_dir, BaseDirectory, FsDirOptions};
+use wasm_bindgen::JsCast;
+use web_sys::{FormData, HtmlFormElement};
+
+use crate::app::{
+    components::fs::fs_dir_option::FsDirOptionInputs,
+    utils::deser_form_data::{deser_form_data, is_on},
+};
+
+const RECURSIVE_INPUT_NAME: &str = "_recursive";
+
+const BASE_DIR_SELECT_NAME: &str = "something_base_dir_dsad";
+
+#[component]
+pub fn CreateDir() -> impl IntoView {
+    let action = Action::<_, anyhow::Result<()>, _>::new_local(|form_data: &FormData| {
+        let form_data = form_data.clone();
+        async move {
+            let mut options: Option<FsDirOptions> =
+                deser_form_data(&form_data).map_err(|e| anyhow::Error::msg(e.to_string()))?;
+            if let Some(opt) = &mut options {
+                opt.recursive = Some(is_on(&form_data, RECURSIVE_INPUT_NAME));
+                opt.dir = form_data
+                    .get(BASE_DIR_SELECT_NAME)
+                    .as_string()
+                    .filter(|e| !e.is_empty())
+                    .and_then(|e| e.parse::<u8>().ok())
+                    .and_then(BaseDirectory::from_repr);
+            }
+            log::debug!("{:#?}", options);
+            create_dir(
+                form_data
+                    .get("path")
+                    .as_string()
+                    .as_ref()
+                    .ok_or(anyhow::Error::msg("path input name is required"))?,
+                options,
+            )
+            .await
+            .map_err(|e| anyhow::Error::msg(e.to_string()))?;
+            Ok(())
+        }
+    });
+    let action_value = action.value_local();
+    let action_pending = action.pending();
+
+    view! {
+        {move || {
+            match action_value.read().as_ref().filter(|_| action_pending.read() == false) {
+                Some(Err(e)) => view! { <p style:color="red">{format!("{e}")}</p> }.into_any(),
+                _ => None::<()>.into_any(),
+            }
+        }}
+        <form on:submit=move |ev| {
+            ev.prevent_default();
+            let form = ev
+                .target()
+                .and_then(|t| t.dyn_into::<HtmlFormElement>().ok())
+                .expect("Should be a form element");
+            let form_data = FormData::new_with_form(&form).expect("Cannot make a form data");
+            action.dispatch_local(form_data);
+        }>
+            <input type="text" name="path" placeholder="Directory path" />
+            <FsDirOptionInputs
+                recursive_input_name=RECURSIVE_INPUT_NAME
+                base_dir_select_name=BASE_DIR_SELECT_NAME
+            />
+            <button type="submit">"Create directory"</button>
+        </form>
+    }
+}
