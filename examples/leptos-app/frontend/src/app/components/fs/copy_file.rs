8b8d6edda19617a13f4e80e8ad1a9910a3307eb7
tonymushah <tonyramanantsalama@gmail.com>
2025-03-14 21:23:07 +0300

    Tested copy file

--- examples/leptos-app/frontend/src/app/components/fs/copy_file.rs
+++ examples/leptos-app/frontend/src/app/components/fs/copy_file.rs
@@ -18,6 +18,13 @@ pub enum ActionType {
     Copy(FormData),
 }
 
+#[derive(Debug, Clone, Copy)]
+pub enum ActionTypeKind {
+    SetSource,
+    SetDestination,
+    Copy,
+}
+
 const APPEND_INPUT_NAME: &str = "_append__";
 const FS_OPTION_INPUT_NAME: &str = "fs_options";
 
@@ -25,79 +32,82 @@ const FS_OPTION_INPUT_NAME: &str = "fs_options";
 pub fn CopyFile() -> impl IntoView {
     let (source, set_source) = signal(None::<String>);
     let (destination, set_destination) = signal(None::<String>);
-    let action = Action::<_, anyhow::Result<()>, _>::new_local(move |input: &ActionType| {
-        let input = input.clone();
-        async move {
-            match input {
-                ActionType::SetSource => {
-                    let src = open(Some(OpenDialogOptions {
-                        title: Some("Source file".into()),
-                        ..Default::default()
-                    }))
-                    .await
-                    .map_err(|e| anyhow::Error::msg(e.to_string()))?
-                    .ok_or(anyhow::anyhow!(
-                        "Empty path was given! Please give proper file"
-                    ))?
-                    .to_vec()
-                    .into_iter()
-                    .next()
-                    .ok_or(anyhow::anyhow!(
-                        "Empty path was given! Please give a proper source file"
-                    ))?;
-                    set_source.set(Some(src));
-                    Ok(())
-                }
-                ActionType::SetDestination => {
-                    let dest = save(Some(SaveDialogOptions {
-                        title: Some("Destination file".into()),
-                        ..Default::default()
-                    }))
-                    .await
-                    .map_err(|e| anyhow::Error::msg(e.to_string()))?
-                    .ok_or(anyhow::anyhow!(
-                        "Empty path was given! Please give us a proper destination file!"
-                    ))?;
-                    set_destination.set(Some(dest));
-                    Ok(())
-                }
-                ActionType::Copy(form_data) => {
-                    let (src, dest) = (
-                        source
-                            .get_untracked()
-                            .ok_or(anyhow::anyhow!("Please give a proper source file!"))?,
-                        destination
-                            .get_untracked()
-                            .ok_or(anyhow::anyhow!("Please give a proper destination file!"))?,
-                    );
-                    let mut options: Option<FsOptions> = deser_form_data(&form_data)
-                        .map_err(|e| anyhow::Error::msg(e.to_string()))?;
-                    if let Some(opt) = &mut options {
-                        opt.append = Some(is_on(&form_data, APPEND_INPUT_NAME));
-                        opt.dir = form_data
-                            .get(FS_OPTION_INPUT_NAME)
-                            .as_string()
-                            .filter(|e| !e.is_empty())
-                            .and_then(|e| e.parse::<u8>().ok())
-                            .and_then(BaseDirectory::from_repr);
-                    }
-                    log::debug!("{:#?}", options);
-                    copy_file(&src, &dest, options)
+    let action =
+        Action::<_, anyhow::Result<ActionTypeKind>, _>::new_local(move |input: &ActionType| {
+            let input = input.clone();
+            async move {
+                match input {
+                    ActionType::SetSource => {
+                        let src = open(Some(OpenDialogOptions {
+                            title: Some("Source file".into()),
+                            ..Default::default()
+                        }))
                         .await
-                        .map_err(|e| anyhow::Error::msg(e.to_string()))?;
-                    Ok(())
+                        .map_err(|e| anyhow::Error::msg(e.to_string()))?
+                        .ok_or(anyhow::anyhow!(
+                            "Empty path was given! Please give proper file"
+                        ))?
+                        .to_vec()
+                        .into_iter()
+                        .next()
+                        .ok_or(anyhow::anyhow!(
+                            "Empty path was given! Please give a proper source file"
+                        ))?;
+                        set_source.set(Some(src));
+                        Ok(ActionTypeKind::SetSource)
+                    }
+                    ActionType::SetDestination => {
+                        let dest = save(Some(SaveDialogOptions {
+                            title: Some("Destination file".into()),
+                            ..Default::default()
+                        }))
+                        .await
+                        .map_err(|e| anyhow::Error::msg(e.to_string()))?
+                        .ok_or(anyhow::anyhow!(
+                            "Empty path was given! Please give us a proper destination file!"
+                        ))?;
+                        set_destination.set(Some(dest));
+                        Ok(ActionTypeKind::SetDestination)
+                    }
+                    ActionType::Copy(form_data) => {
+                        let (src, dest) = (
+                            source
+                                .get_untracked()
+                                .ok_or(anyhow::anyhow!("Please give a proper source file!"))?,
+                            destination
+                                .get_untracked()
+                                .ok_or(anyhow::anyhow!("Please give a proper destination file!"))?,
+                        );
+                        let mut options: Option<FsOptions> = deser_form_data(&form_data)
+                            .map_err(|e| anyhow::Error::msg(e.to_string()))?;
+                        if let Some(opt) = &mut options {
+                            opt.append = Some(is_on(&form_data, APPEND_INPUT_NAME));
+                            opt.dir = form_data
+                                .get(FS_OPTION_INPUT_NAME)
+                                .as_string()
+                                .filter(|e| !e.is_empty())
+                                .and_then(|e| e.parse::<u8>().ok())
+                                .and_then(BaseDirectory::from_repr);
+                        }
+                        log::debug!("{:#?}", options);
+                        copy_file(&src, &dest, options)
+                            .await
+                            .map_err(|e| anyhow::Error::msg(e.to_string()))?;
+                        Ok(ActionTypeKind::Copy)
+                    }
                 }
             }
-        }
-    });
+        });
     let action_data = action.value_local();
     let is_loading = action.pending();
     view! {
         {move || {
             match action_data.read().as_ref().filter(|_| is_loading.read() == false) {
                 Some(Err(e)) => view! { <p style:color="red">{format!("{e}")}</p> }.into_any(),
-                Some(Ok(_)) => view! { <p style:color="green">"File copied!"</p> }.into_any(),
-                None => None::<()>.into_any(),
+                Some(Ok(ActionTypeKind::Copy)) => {
+                    view! { <p style:color="green">"File Copied!"</p> }.into_any()
+                }
+                _ => None::<()>.into_any(),
             }
         }}
         <form on:submit=move |ev| {
