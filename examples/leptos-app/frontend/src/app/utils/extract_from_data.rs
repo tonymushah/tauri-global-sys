2c3efbd4e467a433de6376ec244de427abd36352
tonymushah <tonyramanantsalama@gmail.com>
2025-03-15 18:56:57 +0300

    typo hehe

--- examples/leptos-app/frontend/src/app/utils/extract_from_data.rs
+++ examples/leptos-app/frontend/src/app/utils/extract_from_data.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::JsCast;
 use web_sys::{Event, FormData, HtmlFormElement};
 
-pub fn extract_from_data(ev: &Event) -> FormData {
+pub fn extract_form_data(ev: &Event) -> FormData {
     let form = ev
         .target()
         .and_then(|t| t.dyn_into::<HtmlFormElement>().ok())
