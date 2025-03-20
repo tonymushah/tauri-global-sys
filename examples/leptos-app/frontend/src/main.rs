1bd489adb8e17781bd8fcae94c83009298372664
tonymushah <tonyramanantsalama@gmail.com>
2025-03-21 02:00:17 +0300

    unmount before unload

--- examples/leptos-app/frontend/src/main.rs
+++ examples/leptos-app/frontend/src/main.rs
@@ -1,12 +1,20 @@
 mod app;
 
 use app::*;
-use leptos::prelude::*;
+use js_sys::Function;
+use leptos::{prelude::*, tachys::dom::body};
+use wasm_bindgen::{prelude::Closure, JsCast};
 
 fn main() {
     console_error_panic_hook::set_once();
     console_log::init_with_level(log::Level::Trace).unwrap();
-    mount_to_body(|| {
+    let handle = mount_to(body(), || {
         view! { <App /> }
+    });
+    let listener = Closure::once_into_js(|| {
+        drop(handle);
     })
+    .dyn_into::<Function>()
+    .unwrap();
+    let _ = window().add_event_listener_with_callback("beforeunload", &listener);
 }
